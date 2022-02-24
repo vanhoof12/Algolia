2537107621347f2c7aca4693a762ffdf1b058fac
Casey VanHoof <vanhoofc@mail.gvsu.edu>
2022-02-23 18:49:57 -0700

    Update cameraRenameScript.js

--- cameraRenameScript.js
+++ cameraRenameScript.js
@@ -1,18 +1,20 @@
 function ChangePrices() {
-    const jsonObj= require('./data.json'); 
+    const jsonObj= require('./products.json'); 
     const fs = require('fs');
-    const fileName = './data.json';
+    const fileName = './products.json';
     const file = require(fileName);    
     console.log("hello");
     for (var i=0; i<jsonObj.length; i++) {
-        if (jsonObj[i].categories.includes("Cameras") ||     jsonObj[i].categories.includes("cameras")) {
+        console.log(jsonObj[i].categories)
+        var fullString = jsonObj[i].categories.toString();
+        if (fullString.includes('Cameras') || fullString.includes('cameras')) {
           newPrice = Math.round(jsonObj[i].price / 10) * 10;
           console.log(newPrice)
           jsonObj[i].price = newPrice;
-          fs.writeFileSync(fileName, JSON.stringify(jsonObj))
-          return;
         }
       }
+      fs.writeFileSync(fileName, JSON.stringify(jsonObj))
+      return;
 };
 
 ChangePrices();
